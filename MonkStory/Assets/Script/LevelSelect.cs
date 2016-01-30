a1d5b955b74b8cb381d41ed70345970e65dd986c
Jon <jonathan.hicks@bytecubed.com>
2016-01-29 22:38:17 -0500

    level select code

--- MonkStory/Assets/Script/LevelSelect.cs
+++ MonkStory/Assets/Script/LevelSelect.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class LevelSelect : MonoBehaviour {
 
+  public int level;
+
 	// Use this for initialization
 	void Start () {
-	
+    level = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	  switch (level)
+    {
+      default:
+        break;
+    }
 	}
 }
